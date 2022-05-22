0e0d2a9f2ace1e96cc0a5e293831673a2d53f5b9
Abdus Sattar Bhuiyan <sattar.kuet@gmail.com>
2022-05-22 14:55:12 +0600

    push minor change

--- lib/fragments/bank.dart
+++ lib/fragments/bank.dart
@@ -16,7 +16,7 @@ class _BankState extends State<Bank> {
   final _formKey = GlobalKey<FormState>();
   int bankTypeId = 0;
   List<S2Choice<int>> banks = [];
-  List<Map<int,int>> mobileBanksHashTable = [];
+  Map<int, int> mobileBanksHashTable = {};
   updateDistrictList() async {
     var _bankList = await Service().getBankList();
     for (var i = 0; i < _bankList.length; i++) {
@@ -24,12 +24,12 @@ class _BankState extends State<Bank> {
       String name = _bankList[i]['name'];
       int type = _bankList[i]['type'];
       setState(() {
-        banks
-            .add(S2Choice<int>(value: id, title: name));
-        mobileBanksHashTable.add(Map<id:id,type:type>);
+        banks.add(S2Choice<int>(value: id, title: name));
+        mobileBanksHashTable[id] = type;
       });
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -40,38 +40,43 @@ class _BankState extends State<Bank> {
       body: SingleChildScrollView(
         child: new Form(
           key: _formKey,
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Column(
-              children: [
-                Padding(padding: const EdgeInsets.symmetric(
-                  vertical: 10, horizontal: 20.0),
-                  child:  SmartSelect<int>.single(
-                    placeholder: "নির্বাচন করুন",
-                    modalFilter: true,
-                    modalFilterAuto: true,
-                    tileBuilder: (context, state) => S2Tile<dynamic>(
-                      //https://github.com/akbarpulatov/flutter_awesome_select/blob/master/example/lib/features_single/single_chips.dart
-                      title: const Text(
-                        'কোন ব্যাংক এ টাকা নিতে চান?',
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 20.0),
+                    child: SmartSelect<int>.single(
+                      placeholder: "নির্বাচন করুন",
+                      modalFilter: true,
+                      modalFilterAuto: true,
+                      tileBuilder: (context, state) => S2Tile<dynamic>(
+                        //https://github.com/akbarpulatov/flutter_awesome_select/blob/master/example/lib/features_single/single_chips.dart
+                        title: const Text(
+                          'কোন ব্যাংক এ টাকা নিতে চান?',
+                        ),
+                        value: state.selected?.toWidget() ?? Container(),
+                        onTap: state.showModal,
                       ),
-                      value: state.selected?.toWidget() ?? Container(),
-                      onTap: state.showModal,
+                      title: 'কোন ব্যাংক এ টাকা নিতে চান?',
+                      choiceItems: banks,
+                      onChange: (state) async {
+                        setState(() {
+                          bankTypeId = state.value!;
+                          //updateAreaList();
+                        });
+                      },
+                      selectedValue: bankTypeId,
                     ),
-                    title: 'কোন ব্যাংক এ টাকা নিতে চান?',
-                    choiceItems: banks,
-                    onChange: (state)async {
-                      setState((){ bankTypeId = state.value!;
-                      //updateAreaList();
-                      });
-                    },
-                    selectedValue: bankTypeId,
                   ),
-                ),
-              ],
-            )
-          ],),
+                ],
+              )
+            ],
+          ),
         ),
-        ),
+      ),
       bottomNavigationBar: BottomNavigation(),
       floatingActionButton: floating,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -79,5 +84,4 @@ class _BankState extends State<Bank> {
   }
 }
 
-class id {
-}
+class id {}
--- lib/model/bank.dart
+++ lib/model/bank.dart
@@ -1,14 +1,10 @@
-import 'dart:convert';
-
-import 'package:shared_preferences/shared_preferences.dart';
-
-class Bank{
+class Bank {
   static const int MOBILE_BANK = 1;
   static const int BANK = 0;
   int id = 0;
   String name = '';
   int type = 0;
-  Bank(this.id,this.name, this.type);
+  Bank(this.id, this.name, this.type);
   Bank.fromJson(Map<String, dynamic> bankMap) {
     id = bankMap['id'] ?? 0;
     name = bankMap['name'] ?? '';
@@ -21,28 +17,5 @@ class Bank{
       'name': name,
       'type': type,
     };
-  }
-
-   static Future writeSession(List<Map<int, int>> bank) async {
-    SharedPreferences localStorage = await SharedPreferences.getInstance();
-    localStorage.setString('mobileBanks', json.encode(bank.toJson()));
-  }
-
-  static Future<dynamic> readSession() async {
-    SharedPreferences localStorage = await SharedPreferences.getInstance();
-    var sessionBank = localStorage.getString('mobileBanks');
-    return sessionBank != null
-        ? Bank.fromJson(json.decode(sessionBank))
-        : new Bank(0, '', 0);
-  }
-
-  static Future<int> bankType(int id) async{
-    var mobileBanks = await readSession();
-    for (var mobileBank in mobileBanks){
-       if(mobileBank.id == id){
-         return MOBILE_BANK;
-       }
-    }
-    return BANK;
   }
 }
